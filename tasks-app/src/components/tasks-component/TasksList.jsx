d2e8a92dae7afea9fb9733b4139a6bb58129b5d2
Carolina Albuquerque <carolinamalbuquerque29@gmail.com>
2021-07-10 19:53:57 +0100

    Tasks size indicator

--- tasks-app/src/components/tasks-component/TasksList.jsx
+++ tasks-app/src/components/tasks-component/TasksList.jsx
@@ -5,7 +5,10 @@ import './Tasks.css';
 
 function TasksList(props){
     return <div className="tasksList">
-        <h3 className="listTitle">{props.type}</h3>
+        <div className="listTitle">
+            <h3>{props.type}</h3>
+            <p>{props.data.length}</p>
+        </div>
         {props.data.map(item => <TasksItem key={item.id} text={item.text} state={item.completed}/>)}
     </div>
 }
